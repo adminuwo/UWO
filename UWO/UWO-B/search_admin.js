3dc735b0c1dfd70f5a68801d4bce755981d174be
Its-Sanskar <sanskarbt2@gmail.com>
2026-09-17 16:44:23 +0530

    chore: remove temporary debug and maintenance scripts from UWO-B

--- UWO/UWO-B/search_admin.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('c:/Users/USER/Desktop/uwo/UWO-F/admin.html', 'utf8');
-
-const lines = html.split('\n');
-lines.forEach((line, index) => {
-  if (line.toLowerCase().includes('login')) {
-    console.log(`Line ${index + 1}: ${line.trim()}`);
-  }
-});
